bf1845555024c26031c6b8d8074f0abe06b4ab4c
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-14 16:03:48 -0400

    Shared Dashboard Layout
    Includes the Navbar for all /dashboard routes

---
+++ src/main/ui/src/pages/SharedDashboardLayout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Outlet} from 'react-router-dom'
+import StyledNavbar from '../components/StyledNavbar'
+
+const SharedDashboardLayout = () => {
+  return (
+    <>
+        <StyledNavbar />
+        <Outlet />
+    </>
+  )
+}
+
+export default SharedDashboardLayout
