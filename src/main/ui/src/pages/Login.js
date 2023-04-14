c357f21a05c2d37541d3b322e7700179aed8df7f
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-14 16:03:17 -0400

    Initial Login Page

--- src/main/ui/src/pages/Login.js
+++ src/main/ui/src/pages/Login.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
-const Login = () => {
+const Login = ({ user }) => {
+  if (user) {
+    return <Navigate to='/dashboard' />;
+  }
+
   return (
     <div>Login</div>
   )
