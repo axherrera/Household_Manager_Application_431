46c041317280dd92a09303c66b527c9b967c460c
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-14 16:59:06 -0400

    Mock Data

---
+++ src/main/ui/src/data.js
@@ -0,0 +1,28 @@
+// mock data for demo
+
+export const mockUsers = [
+    {
+        id: 1,
+        name: "mock1",
+        householdID: 1
+    },
+    {
+        id: 2,
+        name: "mock2",
+        householdID: 1
+    },
+    {
+        id: 3,
+        name: "mock3",
+        householdID: 1
+    }
+]
+
+export const mockChores = [
+    {
+        name: "chore 1",
+        dueDate: new Date(),
+        assignedID: 1,
+    },
+
+]
