b369f68fe81f772c3dcdda50ba5031a9a389e424
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-15 13:46:07 -0400

    TODO: Fix Navbar Link Select Styling Bug
    BUG: Styled Navbar stays highlighted on the Home Navlink even if it is not selected

--- src/main/ui/src/components/StyledNavbar.js
+++ src/main/ui/src/components/StyledNavbar.js
@@ -5,6 +5,7 @@ const Navbar = ({logOutFn}) => {
   return (
     <Nav>
       <NavMenu>
+      {/* TODO: *BUG* Styled Navbar stays highlighted on the Home Navlink even if it is not selected */}
         <NavLink to='/dashboard' activeStyle >Home </NavLink>
         <br></br>
         <NavLink to='/dashboard/bills' activeStyle>Bills</NavLink>
