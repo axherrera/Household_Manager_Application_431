a41e8065dffa92bf5fe37c07f54fce9f0b3db849
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-16 14:04:54 -0400

    Mock User Data
    Similar to User DTO:
    - username
    - fname
    - lname
    - household object
        - householdId
        - householdName
    - password (not in User DTO but necessary for mock data user validation)

--- src/main/ui/src/data.js
+++ src/main/ui/src/data.js
@@ -3,18 +3,36 @@
 export const mockUsers = [
     {
         id: 1,
-        name: "mock1",
-        householdID: 1
+        username: "mock1",
+        firstName: "Foo",
+        lastName: "Bar",
+        password: "password1",
+        household: {
+            householdId: 1,
+            householdName: "House 1"
+        }
     },
     {
         id: 2,
-        name: "mock2",
-        householdID: 1
+        username: "mock2",
+        firstName: "Foo",
+        lastName: "Baaz",
+        password: "password2",
+        household: {
+            householdId: 1,
+            householdName: "House 1"
+        }
     },
     {
         id: 3,
-        name: "mock3",
-        householdID: 1
+        username: "mock3",
+        firstName: "Lorem",
+        lastName: "Ipsum",
+        password: "password3",
+        household: {
+            householdId: 1,
+            householdName: "House 1"
+        }
     }
 ]
 
