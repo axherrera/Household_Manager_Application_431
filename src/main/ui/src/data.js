20bbd40561fc6034bb189479b193c0aee8346e67
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-16 14:32:58 -0400

    Initial Mock Bills Data

--- src/main/ui/src/data.js
+++ src/main/ui/src/data.js
@@ -44,3 +44,53 @@ export const mockChores = [
     },
 
 ]
+
+export const mockBills = [
+    {
+        billId: 1,
+        billName: 'Netflix Subscription',
+        billType: 'shows',
+        total: 15.99,
+        notes: 'New movie coming up.',
+        frequency: 'monthly',
+        date: new Date('2023-04-20'),
+        BillHelpers: [
+            {
+                id: 1,
+                amountOwed: 6.00,
+                isPaid: false,
+            },
+            {
+                id: 2,
+                amountOwed: 9.99,
+                isPaid: true,
+            }
+        ]
+    },
+    {
+        billId: 2,
+        billName: 'Mechanic',
+        billType: 'utilities',
+        total: 300.33,
+        notes: 'mechanic is down the road near the theatre',
+        frequency: 'single',
+        date: new Date('2023-05-25'),
+        BillHelpers: [
+            {
+                id: 1,
+                amountOwed: 100.11,
+                isPaid: true,
+            },
+            {
+                id: 2,
+                amountOwed: 100.11,
+                isPaid: true,
+            },
+            {
+                id: 3,
+                amountOwed: 100.11,
+                isPaid: false,
+            }
+        ]
+    },
+]
