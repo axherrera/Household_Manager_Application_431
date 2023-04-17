2ecf3540ad0c612e1234a64e7d3df1abc9063250
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-17 13:50:15 -0400

    Centering Bills Content CSS

--- src/main/ui/src/pages/dashboard/bills/Routes.js
+++ src/main/ui/src/pages/dashboard/bills/Routes.js
@@ -3,16 +3,19 @@ import { Outlet, Route, Routes } from 'react-router-dom/dist/umd/react-router-do
 import Edit from './Edit'
 import Home from './Home'
 import SingleBill from './SingleBill'
+import './Bills.css'
 
 const BillsRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      {/* TODO: Add Protected Bill Checks to See if getting Single Bill still exists
-      Use outlet context: https://reactrouter.com/en/6.4.4/hooks/use-outlet-context */}
-      <Route path=":billId" element={<Outlet />}>
-        <Route index element={<SingleBill />} />
-        <Route path="edit" element={<Edit />} />
+      <Route path="/*" element={<div style={{ textAlign: 'center' }}><div className='bills-content'><Outlet /></div></div>}>
+        <Route index element={<Home />} />
+        {/* TODO: Add Protected Bill Checks to See if getting Single Bill still exists
+        Use outlet context: https://reactrouter.com/en/6.4.4/hooks/use-outlet-context */}
+        <Route path=":billId" element={<Outlet />}>
+          <Route index element={<SingleBill />} />
+          <Route path="edit" element={<Edit />} />
+        </Route>
       </Route>
     </Routes>
   )
