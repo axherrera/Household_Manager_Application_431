a69af79c28f6f5bc0ce41a1eb4f36bf003d95c90
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-17 13:32:36 -0400

    Nested Route for Index Unnecessary

--- src/main/ui/src/pages/dashboard/bills/Routes.js
+++ src/main/ui/src/pages/dashboard/bills/Routes.js
@@ -7,14 +7,12 @@ import SingleBill from './SingleBill'
 const BillsRoutes = () => {
   return (
     <Routes>
-      <Route path="/*" element={<Outlet />}>
-        <Route index element={<Home />} />
-        {/* TODO: Add Protected Bill Checks to See if getting Single Bill still exists
-        Use outlet context: https://reactrouter.com/en/6.4.4/hooks/use-outlet-context */}
-        <Route path=":billId" element={<Outlet />}>
-          <Route index element={<SingleBill />} />
-          <Route path="edit" element={<Edit />} />
-        </Route>
+      <Route path="/" element={<Home />} />
+      {/* TODO: Add Protected Bill Checks to See if getting Single Bill still exists
+      Use outlet context: https://reactrouter.com/en/6.4.4/hooks/use-outlet-context */}
+      <Route path=":billId" element={<Outlet />}>
+        <Route index element={<SingleBill />} />
+        <Route path="edit" element={<Edit />} />
       </Route>
     </Routes>
   )
