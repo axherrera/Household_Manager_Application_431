98df75e4313fce9b4f478dea05f134cfe1dbe762
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-17 13:55:50 -0400

    Underlining Single Bill Headers

--- src/main/ui/src/pages/dashboard/bills/SingleBill.js
+++ src/main/ui/src/pages/dashboard/bills/SingleBill.js
@@ -10,11 +10,11 @@ const SingleBill = () => {
     return (
         <>
             <h3><b>{bill.name} Bill</b></h3>
-            <h5>total: {bill.total}</h5>
-            <h5>frequency: {bill.frequency}</h5>
-            <h5>notes:</h5>
+            <h5><u>total:</u> {bill.total}</h5>
+            <h5><u>frequency:</u> {bill.frequency}</h5>
+            <h5><u>notes:</u></h5>
             <div style={{ fontSize: 12 }}>{bill.notes}</div>
-            <h5>due date: {bill.date.toString()}</h5>
+            <h5><u>due date:</u> {bill.date.toString()}</h5>
             <br></br>
             <Link to='/dashboard/bills'> back to all bills</Link>
             <br></br>
