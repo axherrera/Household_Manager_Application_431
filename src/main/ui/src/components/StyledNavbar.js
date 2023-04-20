c2337f5d730827aac2cb4c69927bcaf3807b651e
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-19 21:48:25 -0400

    Remove ActiveStyle in NavLink

--- src/main/ui/src/components/StyledNavbar.js
+++ src/main/ui/src/components/StyledNavbar.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import {Nav, NavLink, NavMenu} from './NavbarElements'
+import { Nav, NavLink, NavMenu } from './NavbarElements'
 
-const Navbar = ({logOutFn}) => {
+const Navbar = ({ logOutFn }) => {
   return (
     <Nav>
       <NavMenu>
-      {/* TODO: *BUG* Styled Navbar stays highlighted on the Home Navlink even if it is not selected */}
-        <NavLink to='/dashboard' activeStyle >Home </NavLink>
+        {/* TODO: *BUG* Styled Navbar stays highlighted on the Home Navlink even if it is not selected */}
+        <NavLink to='/dashboard' >Home </NavLink>
         <br></br>
-        <NavLink to='/dashboard/bills' activeStyle>Bills</NavLink>
+        <NavLink to='/dashboard/bills' >Bills</NavLink>
         <br></br>
-        <NavLink to='/dashboard/chores' activeStlye>Chores</NavLink>
+        <NavLink to='/dashboard/chores' >Chores</NavLink>
         <br></br>
-        <NavLink to='/' activeStlye onClick={logOutFn}>Logout</NavLink>
+        <NavLink to='/' onClick={logOutFn}>Logout</NavLink>
       </NavMenu>
     </Nav>
   )
