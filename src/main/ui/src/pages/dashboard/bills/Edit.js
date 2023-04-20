756a45d28315097919e9e63406e1d328b471224b
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-19 21:53:02 -0400

    HandleMockSubmit for Edit Bill

--- src/main/ui/src/pages/dashboard/bills/Edit.js
+++ src/main/ui/src/pages/dashboard/bills/Edit.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Form from './Form'
 import { useOutletContext } from 'react-router-dom';
+import { LoginContext } from '../../../contexts/LoginContext';
+
+const HandleMockSubmit = (editedBill, setBills) => {
+    setBills(bills => {
+        return bills.map(bill => {
+            if (bill.id === editedBill.id) {
+                return editedBill;
+            }
+            return bill;
+        })
+    })
+}
+
 
 const Edit = () => {
     const { bill } = useOutletContext();
-    
+
+    const { setBills } = useContext(LoginContext);
+
     const handleSubmit = (editedBill) => {
-        // TODO: do validation and edit changes
+        if (process.env.REACT_APP_MOCK) {
+            HandleMockSubmit(editedBill, setBills);
+        }
     }
 
     return (
-        <Form bill={bill} handleSubmit={handleSubmit}/>
+        <Form bill={bill} handleSubmit={handleSubmit} />
     )
 }
 
-export default Edit
+
+export default Edit;
