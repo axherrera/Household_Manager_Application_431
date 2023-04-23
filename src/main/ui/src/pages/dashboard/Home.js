5400384aebff94282440f0e1887b03ef16bfe85d
Kenneth Salanga <kennethsalanga@gmail.com>
2023-04-23 14:49:25 -0400

    Homepage Redesign

--- src/main/ui/src/pages/dashboard/Home.js
+++ src/main/ui/src/pages/dashboard/Home.js
@@ -1,12 +1,34 @@
-import { React, useContext } from 'react'
+import { React, useContext, useEffect, useState } from 'react'
 import { LoginContext } from '../../contexts/LoginContext';
+import { Typography, Fade, Paper } from '@mui/material';
 
 const Home = () => {
   const { user } = useContext(LoginContext);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+  }, []);
 
   return (
-    <div>Welcome, {user.firstName} to {user.Household.name}!</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "80vh",
+      }}
+    >
+    <Fade in={isVisible} timeout={1000}>
+      <Paper elevation={3} style={{ padding: "2rem" }}>
+        <Typography variant="h3" align="center">
+          Welcome, {user.firstName} 👋
+        </Typography>
+      </Paper>
+    </Fade>
+    </div>
+  );
+
 }
 
 export default Home
