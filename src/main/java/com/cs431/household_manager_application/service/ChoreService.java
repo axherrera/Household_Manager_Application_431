b7fa5054de62b7bb44cc8027b83bb7b810ba6dfb
Tyler <tylersarno21@gmail.com>
2023-04-28 15:23:25 -0400

    chore endpoints completed but not tested

--- src/main/java/com/cs431/household_manager_application/service/ChoreService.java
+++ src/main/java/com/cs431/household_manager_application/service/ChoreService.java
@@ -2,13 +2,19 @@ package com.cs431.household_manager_application.service;
 
 import com.cs431.household_manager_application.model.Chore;
 import com.cs431.household_manager_application.model.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 public interface ChoreService {
-    public Chore saveChore(Chore chore);
-    public List<Chore> getAllChores();
-    public List<Chore> getUserChores(User user);
-    public List<Chore> rotateChores();
-    public List<Chore> checkExpiration();
+    Chore saveChore(Chore chore);
+    List<Chore> getAllChores(Long id);
+    List<Chore> getUserChores(User user);
+    List<Chore> rotateChores();
+    List<Chore> checkExpiration();
+    Boolean updateChore(Long choreId, Chore updated);
+    Boolean deleteChore(Long id);
+    Chore editChore(Long choreId, Chore newChore);
+    Boolean markAsCompleted(Long choreId, Chore chore);
 }
