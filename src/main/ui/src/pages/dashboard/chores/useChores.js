241bc3a0d96b224fed172f4c539c9b4534be526f
Kenneth Salanga <kennethsalanga@gmail.com>
2023-05-01 23:39:25 -0400

    Chores Requests

--- src/main/ui/src/pages/dashboard/chores/useChores.js
+++ src/main/ui/src/pages/dashboard/chores/useChores.js
@@ -1,22 +1,33 @@
 import { LoginContext } from '../../../contexts/LoginContext'
 import { useContext } from "react";
 import { useNavigate } from 'react-router-dom/dist/umd/react-router-dom.development';
-
+import axios from 'axios';
 const useChores = () => {
     const {user, chores, setChores} = useContext(LoginContext);
     const houseId = user.Household.id;
-  
-    const getAllMockChores = () => {
-       return chores.filter(chore => chore.houseId=== houseId)
-    };
-    const getAllChores = () => {
-        if (process.env.REACT_APP_MOCK) {
-            return getAllMockChores();
+    const getChore = async (id) => {
+        const url = /households/${houseId}/chores/${id};
+        try {
+            const response = await axios.get(url);
+            return response.data;
+        } catch (error) {
+            console.log('error', error)
+        }
+        return null;
+    }
+
+    const getAllChores = async () => {
+        const url = /households/${houseId}/chores;
+        try {
+            const response = await axios.get(url);
+            //console.log(response.data)
+            return response.data;
+        } catch (error) {
+            console.log('error', error)
         }
         return [];
-    };
 
-
-    return {getAllChores}
+        return {getAllChores}
+    }
 }
 export default useChores
